5ad9318cdafece8170a3e94cc50b439fd8299659
Simon Josefsson <simon@josefsson.org>
2002-11-14 08:54:59 +0000

    Use autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,3 @@
 #!/bin/sh -x
-ACLOCAL=${ACLOCAL:-aclocal}; export ACLOCAL
-AUTOMAKE=${AUTOMAKE:-automake}; export AUTOMAKE
-AUTOCONF=${AUTOCONF:-autoconf}; export AUTOCONF
-LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}; export LIBTOOLIZE
-AUTOHEADER=${AUTOHEADER:-autoheader}; export AUTOHEADER
-
-rm -vf config.cache &&
-rm -rvf autom4te.cache &&
-$ACLOCAL -I .
-$LIBTOOLIZE --force --automake
-$ACLOCAL -I .
-$AUTOCONF
-$AUTOMAKE --gnits --add-missing
-$AUTOHEADER
+autoreconf --install --force --verbose
 : 'You can now run CFLAGS=-g ./configure --disable-shared and then make.'
