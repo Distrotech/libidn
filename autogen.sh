2e20d334b392398ee239ea3b68a9f9c482696481
Simon Josefsson <simon@josefsson.org>
2002-11-30 16:13:29 +0000

    Add --enable-maintainer-mode.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-: 'You can now run ./configure CFLAGS=-g --disable-shared and then make.'
+: 'You can now run ./configure CFLAGS=-g --enable-maintainer-mode --disable-shared && make.'
