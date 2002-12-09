a71325f31011822987b10b92bada07c062e03ef3
Simon Josefsson <simon@josefsson.org>
2002-12-09 11:57:06 +0000

    Suggest dmalloc.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-: 'You can now run ./configure CFLAGS=-g --enable-maintainer-mode --disable-shared && make.'
+: 'You can now run ./configure CFLAGS=-g --enable-maintainer-mode --with-dmalloc --disable-shared && make.'
