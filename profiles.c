fc3868cb9f62c83922c3051cbd7a57345736058e
Simon Josefsson <simon@josefsson.org>
2003-02-19 18:56:50 +0000

    Don't normalize or map ASCII space for XMPP nodeprep.  Tiny patch from Travis Shirk <TShirk@jabber.com>.

--- profiles.c
+++ profiles.c
@@ -204,10 +204,6 @@ Stringprep_profile stringprep_xmpp_nodeprep[] = {
   ,
   {STRINGPREP_MAP_TABLE, 0, stringprep_generic_B_2, "B.2"}
   ,
-  {STRINGPREP_NFKC, 0, 0, "NFKC"}
-  ,
-  {STRINGPREP_PROHIBIT_TABLE, 0, stringprep_generic_C_1_1, "C.1.1"}
-  ,
   {STRINGPREP_PROHIBIT_TABLE, 0, stringprep_generic_C_1_2, "C.1.2"}
   ,
   {STRINGPREP_PROHIBIT_TABLE, 0, stringprep_generic_C_2_1, "C.2.1"}
