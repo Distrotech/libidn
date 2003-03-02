ddcd9fdce46c9b2d57bfef3a6c58a67f7eb5cdf3
Simon Josefsson <simon@josefsson.org>
2003-03-02 13:14:59 +0000

    Fix ct. Add example.

--- contrib/web/idn.php
+++ contrib/web/idn.php
@@ -2,7 +2,7 @@
 
   <head>
     <title>Try GNU Libidn</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php print $charset ? $charset : "ISO-8859-1" ?>" />
+    <meta http-equiv='Content-Type' content='text/html; charset="<?php print $charset ? $charset : "ISO-8859-1" ?>"' />
   </head>
 
   <body>
@@ -288,6 +288,7 @@
 
     <ul>
 	<li><a href="http://josefsson.org/idn.php?data=r%E4ksm%F6rg%E5s.josefsson.org&mode=toascii&charset=ISO-8859-1">ISO-8859-1 example</a>
+	<li><a href="http://josefsson.org/idn.php?data=r%84ksm%94rg%86s.josefsson.org&mode=toascii&charset=IBM850">IBM850 (aka codepage 850) example (same as previous)</a>
 	<li><a href="http://josefsson.org/idn.php?data=%E2%82%ACcu&mode=toascii&charset=UTF-8">UTF-8 Euro example</a>
 	<li><a href="http://josefsson.org/idn.php?data=%A4cu&mode=toascii&charset=ISO-8859-15">ISO-8859-15 Euro example (same as previous)</a>
 	<li><a href="http://josefsson.org/idn.php?data=%B0%C2%BC%BC%C6%E0%C8%FE%B7%C3-with-SUPER-MONKEYS&mode=toascii&charset=EUC-JP">EUC-JP example</a>
