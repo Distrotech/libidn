091750a33a06fd832915751f29bb468a29d342f3
Simon Josefsson <simon@josefsson.org>
2003-03-02 12:30:44 +0000

    Bug reporting.

--- contrib/web/idn.php
+++ contrib/web/idn.php
@@ -16,6 +16,12 @@
     stringprep implementation with support for several stringprep
     profiles.
 
+    <p>Report bugs to <A
+    HREF="mailto:bug-libidn@gnu.org">bug-libidn@gnu.org</A>, but first
+    please make sure your browser really is encoding the data you type
+    in the charset you select.  If not, an error is the proper
+    response.
+
     <hr>
     <h2>Input</h2>
     <form>
