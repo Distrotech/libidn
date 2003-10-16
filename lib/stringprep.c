3402fe972544cb8e7ba01662711a17bd121944ed
Simon Josefsson <simon@josefsson.org>
2003-10-16 00:41:07 +0000

    (stringprep_profile): Loop around stringprep return STRINGPREP_TOO_SMALL_BUFFER and increase buffer size.

--- lib/stringprep.c
+++ lib/stringprep.c
@@ -416,8 +416,8 @@ stringprep_profile (const char *in,
 		    Stringprep_profile_flags flags)
 {
   Stringprep_profiles *p;
-  char *str;
-  size_t len;
+  char *str = NULL;
+  size_t len = strlen (in) + 1;
   int rc;
 
   for (p = &stringprep_profiles[0]; p->name; p++)
@@ -427,14 +427,20 @@ stringprep_profile (const char *in,
   if (!p || !p->name || !p->tables)
     return STRINGPREP_UNKNOWN_PROFILE;
 
-  len = strlen (in) + BUFSIZ;
-  str = (char *) malloc (len);
-  if (str == NULL)
-    return STRINGPREP_MALLOC_ERROR;
+  do
+    {
+      if (str)
+	free (str);
+      str = (char *) malloc (len);
+      if (str == NULL)
+	return STRINGPREP_MALLOC_ERROR;
 
-  strcpy (str, in);
+      strcpy (str, in);
 
-  rc = stringprep (str, len, flags, p->tables);
+      rc = stringprep (str, len, flags, p->tables);
+      len += 50;
+    }
+  while (rc == STRINGPREP_TOO_SMALL_BUFFER);
 
   if (rc == STRINGPREP_OK)
     *out = str;
