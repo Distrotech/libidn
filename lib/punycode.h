f4b7cda5c1703ff403642655e1976cbe66a9c52e
Simon Josefsson <simon@josefsson.org>
2003-10-16 02:46:08 +0000

    Sync with latest draft, but change return types to 'int' to be backwards compatible.

--- lib/punycode.h
+++ lib/punycode.h
@@ -70,80 +70,148 @@ extern "C"
 #include <stddef.h>		/* size_t */
 #include <idn-int.h>		/* uint32_t */
 
-  typedef enum
-  {
-    PUNYCODE_SUCCESS = 0,
-    PUNYCODE_BAD_INPUT,		/* Input is invalid.                       */
-    PUNYCODE_BIG_OUTPUT,	/* Output would exceed the space provided. */
-    PUNYCODE_OVERFLOW		/* Input needs wider integers to process.  */
-  } Punycode_status;
-
-  /* For RFC compatibility. */
   enum punycode_status
   {
-    punycode_success = PUNYCODE_SUCCESS,
-    punycode_bad_input = PUNYCODE_BAD_INPUT,
-    punycode_big_output = PUNYCODE_BIG_OUTPUT,
-    punycode_overflow = PUNYCODE_OVERFLOW
+    punycode_success = 0,
+    punycode_bad_input = 1,	/* Input is invalid.                       */
+    punycode_big_output = 2,	/* Output would exceed the space provided. */
+    punycode_overflow = 3	/* Wider integers needed to process input. */
   };
+
+  typedef enum
+  {
+    PUNYCODE_SUCCESS = punycode_success,
+    PUNYCODE_BAD_INPUT = punycode_bad_input,
+    PUNYCODE_BIG_OUTPUT = punycode_big_output,
+    PUNYCODE_OVERFLOW = punycode_overflow
+  } Punycode_status;
+
+/* punycode_uint needs to be unsigned and needs to be */
+/* at least 26 bits wide.                             */
 
   typedef uint32_t punycode_uint;
 
-  int punycode_encode (size_t input_length,
-		       const punycode_uint input[],
-		       const unsigned char case_flags[],
-		       size_t * output_length, char output[]);
+  extern int punycode_encode (size_t,	/* input_length  */
+			      const punycode_uint[],	/* input         */
+			      const unsigned char[],	/* case_flags    */
+			      size_t *,	/* output_length */
+			      char[]	/* output        */
+    );
 
-  /* punycode_encode() converts Unicode to Punycode.  The input     */
-  /* is represented as an array of Unicode code points (not code    */
-  /* units; surrogate pairs are not allowed), and the output        */
-  /* will be represented as an array of ASCII code points.  The     */
-  /* output string is *not* null-terminated; it will contain        */
-  /* zeros if and only if the input contains zeros.  (Of course     */
-  /* the caller can leave room for a terminator and add one if      */
-  /* needed.)  The input_length is the number of code points in     */
-  /* the input.  The output_length is an in/out argument: the       */
-  /* caller passes in the maximum number of code points that it     */
-  /* can receive, and on successful return it will contain the      */
-  /* number of code points actually output.  The case_flags array   */
-  /* holds input_length boolean values, where nonzero suggests that */
-  /* the corresponding Unicode character be forced to uppercase     */
-  /* after being decoded (if possible), and zero suggests that      */
-  /* it be forced to lowercase (if possible).  ASCII code points    */
-  /* are encoded literally, except that ASCII letters are forced    */
-  /* to uppercase or lowercase according to the corresponding       */
-  /* uppercase flags.  If case_flags is a null pointer then ASCII   */
-  /* letters are left as they are, and other code points are        */
-  /* treated as if their uppercase flags were zero.  The return     */
-  /* value can be any of the punycode_status values defined above   */
-  /* except punycode_bad_input; if not punycode_success, then       */
-  /* output_size and output might contain garbage.                  */
+/*
+    punycode_encode() converts a sequence of code points (presumed to be
+    Unicode code points) to Punycode.
 
-  int punycode_decode (size_t input_length,
-		       const char input[],
-		       size_t * output_length,
-		       punycode_uint output[], unsigned char case_flags[]);
+    Input arguments (to be supplied by the caller):
 
-  /* punycode_decode() converts Punycode to Unicode.  The input is  */
-  /* represented as an array of ASCII code points, and the output   */
-  /* will be represented as an array of Unicode code points.  The   */
-  /* input_length is the number of code points in the input.  The   */
-  /* output_length is an in/out argument: the caller passes in      */
-  /* the maximum number of code points that it can receive, and     */
-  /* on successful return it will contain the actual number of      */
-  /* code points output.  The case_flags array needs room for at    */
-  /* least output_length values, or it can be a null pointer if the */
-  /* case information is not needed.  A nonzero flag suggests that  */
-  /* the corresponding Unicode character be forced to uppercase     */
-  /* by the caller (if possible), while zero suggests that it be    */
-  /* forced to lowercase (if possible).  ASCII code points are      */
-  /* output already in the proper case, but their flags will be set */
-  /* appropriately so that applying the flags would be harmless.    */
-  /* The return value can be any of the punycode_status values      */
-  /* defined above; if not punycode_success, then output_length,    */
-  /* output, and case_flags might contain garbage.  On success, the */
-  /* decoder will never need to write an output_length greater than */
-  /* input_length, because of how the encoding is defined.          */
+        input_length
+            The number of code points in the input array and the number
+            of flags in the case_flags array.
+
+        input
+            An array of code points.  They are presumed to be Unicode
+            code points, but that is not strictly REQUIRED.  The
+            array contains code points, not code units.  UTF-16 uses
+            code units D800 through DFFF to refer to code points
+            10000..10FFFF.  The code points D800..DFFF do not occur in
+            any valid Unicode string.  The code points that can occur in
+            Unicode strings (0..D7FF and E000..10FFFF) are also called
+            Unicode scalar values.
+
+        case_flags
+            A null pointer or an array of boolean values parallel to
+            the input array.  Nonzero (true, flagged) suggests that the
+            corresponding Unicode character be forced to uppercase after
+            being decoded (if possible), and zero (false, unflagged)
+            suggests that it be forced to lowercase (if possible).
+            ASCII code points (0..7F) are encoded literally, except that
+            ASCII letters are forced to uppercase or lowercase according
+            to the corresponding case flags.  If case_flags is a null
+            pointer then ASCII letters are left as they are, and other
+            code points are treated as unflagged.
+
+    Output arguments (to be filled in by the function):
+
+        output
+            An array of ASCII code points.  It is *not* null-terminated;
+            it will contain zeros if and only if the input contains
+            zeros.  (Of course the caller can leave room for a
+            terminator and add one if needed.)
+
+    Input/output arguments (to be supplied by the caller and overwritten
+    by the function):
+
+        output_length
+            The caller passes in the maximum number of ASCII code points
+            that it can receive.  On successful return it will contain
+            the number of ASCII code points actually output.
+
+    Return value:
+
+        Can be any of the punycode_status values defined above except
+        punycode_bad_input.  If not punycode_success, then output_size
+        and output might contain garbage.
+*/
+
+  extern int punycode_decode (size_t,	/* input_length  */
+			      const char[],	/* input         */
+			      size_t *,	/* output_length */
+			      punycode_uint[],	/* output        */
+			      unsigned char[]	/* case_flags    */
+    );
+
+/*
+    punycode_decode() converts Punycode to a sequence of code points
+    (presumed to be Unicode code points).
+
+    Input arguments (to be supplied by the caller):
+
+        input_length
+            The number of ASCII code points in the input array.
+
+        input
+            An array of ASCII code points (0..7F).
+
+    Output arguments (to be filled in by the function):
+
+        output
+            An array of code points like the input argument of
+            punycode_encode() (see above).
+
+        case_flags
+            A null pointer (if the flags are not needed by the caller)
+            or an array of boolean values parallel to the output array.
+            Nonzero (true, flagged) suggests that the corresponding
+            Unicode character be forced to uppercase by the caller (if
+            possible), and zero (false, unflagged) suggests that it
+            be forced to lowercase (if possible).  ASCII code points
+            (0..7F) are output already in the proper case, but their
+            flags will be set appropriately so that applying the flags
+            would be harmless.
+
+    Input/output arguments (to be supplied by the caller and overwritten
+    by the function):
+
+        output_length
+            The caller passes in the maximum number of code points
+            that it can receive into the output array (which is also
+            the maximum number of flags that it can receive into the
+            case_flags array, if case_flags is not a null pointer).  On
+            successful return it will contain the number of code points
+            actually output (which is also the number of flags actually
+            output, if case_flags is not a null pointer).  The decoder
+            will never need to output more code points than the number
+            of ASCII code points in the input, because of the way the
+            encoding is defined.  The number of code points output
+            cannot exceed the maximum possible value of a punycode_uint,
+            even if the supplied output_length is greater than that.
+
+    Return value:
+
+        Can be any of the punycode_status values defined above.  If not
+        punycode_success, then output_length, output, and case_flags
+        might contain garbage.
+*/
 
 #ifdef __cplusplus
 }
