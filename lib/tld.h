10e0f485945aac7a7f5ab70906d6546bb65d56b4
Simon Josefsson <simon@josefsson.org>
2004-02-06 20:15:38 +0000

    Fix Tld_table.

--- lib/tld.h
+++ lib/tld.h
@@ -50,7 +50,7 @@ extern "C"
     char *name;			/* TLD name, e.g., "no". */
     char *version;		/* Version string from TLD file. */
     size_t nvalid;		/* Number of entries in data. */
-    const Tld_table_element *valid;	/* Sorted array of valid code points. */
+    Tld_table_element *valid[];	/* Sorted array of valid code points. */
   };
   typedef struct Tld_table Tld_table;
 
