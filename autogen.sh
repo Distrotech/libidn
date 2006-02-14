00e64a99069b789c7f4e8ddb03a9e5c3141b8c95
Simon Josefsson <simon@josefsson.org>
2006-02-14 11:31:30 +0000

    Remove.

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-cat<<EOF > doc/Makefile.gdoc
-gdoc_MANS =
-gdoc_TEXINFOS =
-EOF
-gtkdocize
-autoreconf --install --force
-: 'Run "./configure --enable-java --enable-gtk-doc && make" now.'
