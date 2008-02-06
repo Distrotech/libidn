e50a889b9765bf716109c97860d41673fae1c8b4
Simon Josefsson <simon@josefsson.org>
2008-02-06 18:13:49 +0100

    Brace expansion is not POSIX portable.

--- maint-cfg.mk
+++ maint-cfg.mk
@@ -28,7 +28,7 @@ doc/Makefile.gdoc:
 
 autoreconf: doc/Makefile.gdoc
 	test -f ./configure || autoreconf --install
-	mv build-aux/config.rpath{-,}
+	mv build-aux/config.rpath- build-aux/config.rpath
 
 bootstrap: autoreconf
 	./configure $(CFGFLAGS)
