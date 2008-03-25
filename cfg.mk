40e29a00cbaaf4f1f17b2f0ebd1bd1a2b4746540
Simon Josefsson <simon@josefsson.org>
2008-03-25 21:57:46 +0100

    Need config.rpath workaround here.

--- cfg.mk
+++ cfg.mk
@@ -27,6 +27,7 @@ doc/Makefile.gdoc:
 	printf "gdoc_MANS =\ngdoc_TEXINFOS =\n" > doc/Makefile.gdoc
 
 autoreconf: doc/Makefile.gdoc
+	mv build-aux/config.rpath build-aux/config.rpath-
 	test -f ./configure || autoreconf --install
 	mv build-aux/config.rpath- build-aux/config.rpath
 
