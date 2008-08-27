5d36cb247d0e916d9a12ababad53cdf7da14d44e
Simon Josefsson <simon@josefsson.org>
2008-08-27 10:13:35 +0200

    Declare strverscmp.

--- win32/include/gen-win32-headers.pl
+++ win32/include/gen-win32-headers.pl
@@ -69,6 +69,8 @@ if(@ARGV == 0) {
 #define strcasecmp stricmp
 #define strncasecmp strnicmp
 
+extern int strverscmp (const char *, const char *);
+
 #define LOCALEDIR "."
 
 #if _MSC_VER && !__cplusplus
