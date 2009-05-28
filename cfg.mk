960def128145648ae7a2c2c5ad48174d26dfd5b4
Simon Josefsson <simon@josefsson.org>
2009-05-28 22:52:18 +0200

    Improve warning handling.

--- cfg.mk
+++ cfg.mk
@@ -15,8 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-WFLAGS ?= WARN_CFLAGS=-Werror
-CFGFLAGS ?= --enable-java --enable-gtk-doc $(WFLAGS)
+WFLAGS ?= --enable-gcc-warnings
+ADDFLAGS ?=
+CFGFLAGS ?= --enable-java --enable-gtk-doc $(ADDFLAGS) $(WFLAGS)
 
 ifeq ($(.DEFAULT_GOAL),abort-due-to-no-makefile)
 .DEFAULT_GOAL := bootstrap
